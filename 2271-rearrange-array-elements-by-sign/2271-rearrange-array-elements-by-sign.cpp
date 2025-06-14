6fbee4e9d1e6ab0f0112ff2347c3a5059d186f6c
realcyclopse <149539019+realcyclopse@users.noreply.github.com>
2025-06-15 02:40:38 +0530

    Time: 6 ms (64.77%), Space: 128.2 MB (79.41%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> positive;
-        vector<int> negative;
-        vector<int> res;
+        vector<int> res(nums.size());
+        int a=0;
+        int b=1;
         for(int i = 0; i<nums.size();i++)
         {
             if(nums[i]>0)
             {
-                positive.push_back(nums[i]);
+                res[a]=nums[i];
+                a = a + 2;
             }
             else
             {
-                negative.push_back(nums[i]);
+                res[b]=nums[i];
+                b = b + 2; 
             }
         }
-        for(int i = 0; i<nums.size()/2;i++)
-        {
-            res.push_back(positive[i]);
-            res.push_back(negative[i]);
-        }
         return res;
+        
     }
 };
